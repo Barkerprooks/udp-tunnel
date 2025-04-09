f8d5a9bd2abb69a9c3b2d7d7172ca4eaeb4ab1e4
BarkerProoks <jonparkerbrooks@gmail.com>
2025-04-09 20:31:34 +0000

    mmm

--- udptun.py
+++ udptun.py
@@ -269,11 +269,6 @@ class LocalTunnelProtocol(DatagramProtocol):
         v_print(self.verbose, f"local tunnel recv: service <- tunnel <- client\n{data}")
         self.forward.sendto(data) # forward it directly to the service
 
-    def connection_lost(self, exc):
-        print("server disconnect worked!")
-        # close the linked transport
-        self.forward.close()
-
 
 class LocalForwardProtocol(DatagramProtocol):
     """
@@ -376,8 +371,10 @@ async def run_local_loop(forward_addr: tuple[str, int], connect_addr: tuple[str,
             for port in router_protocol.expired_connections:
                 print(f"closing the tunnel for port {port}")
                 tunnel_transport, forward_transport = connections[port]
-                transports.remove(forward_transport)
-                transports.remove(tunnel_transport)
+                for transport in transports:
+                    print(transport, forward_transport)
+                # transports.remove(forward_transport)
+                # transports.remove(tunnel_transport)
                 forward_transport.close()
                 tunnel_transport.close()
             
