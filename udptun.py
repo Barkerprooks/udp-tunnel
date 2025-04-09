d8206bd3c3a19b0cf4c14291cc46651e88d75c4f
BarkerProoks <jonparkerbrooks@gmail.com>
2025-04-09 20:34:30 +0000

    damn

--- udptun.py
+++ udptun.py
@@ -371,9 +371,7 @@ async def run_local_loop(forward_addr: tuple[str, int], connect_addr: tuple[str,
             for port in router_protocol.expired_connections:
                 print(f"closing the tunnel for port {port}")
                 tunnel_transport, forward_transport = connections[port]
-                for transport in transports:
-                    print(transport, forward_transport)
-                # transports.remove(forward_transport)
+                transports.remove(forward_transport)
                 # transports.remove(tunnel_transport)
                 forward_transport.close()
                 tunnel_transport.close()
